12f2a72ffdc56d93c6bff09dd13ff0565c74eeb6
nicolasrls <nicolaufacul@gmail.com>
2019-12-09 21:08:42 -0300

    Aula 09/12

--- Circuitos Lógicos/FPGA/Projeto0312.1/conta10s.vhd
+++ Circuitos Lógicos/FPGA/Projeto0312.1/conta10s.vhd
@@ -2,7 +2,7 @@ library IEEE;
 use ieee.std_logic_1164.all;
  
 entity conta10s is
-port (clock1, reset: in std_logic;
+port (clock1, reset, sobedesce: in std_logic;
         HEX0: out std_logic_vector (0 to 6));
 end conta10s;
  
@@ -18,18 +18,21 @@ architecture arquitetura of conta10s is
               q: out std_logic);
     end component;
    
-    component conta_decada is
-    port (clk:in std_logic;
-         reset: in std_logic;
-         q: out std_logic_vector(3 downto 0));
+    component conta_updown is
+    port (clk: in std_logic;
+     reset: in std_logic;
+     sobedesce: in std_logic;
+     carry_out: out std_logic;
+     q: out std_logic_vector(3 downto 0));
     end component;
    
     signal chupitilha: std_logic;
     signal querrenca: std_logic_vector (3 downto 0);
+	 
    
 begin
 talita: clock port map (clock1, chupitilha);
-feliperenato: conta_decada port map (chupitilha, reset, querrenca);
+feliperenato: conta_updown port map(chupitilha, reset, sobedesce, open ,querrenca);
 caleb: displaycvector port map (querrenca, HEX0);
  
 end arquitetura;
