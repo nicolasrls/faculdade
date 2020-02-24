9b47eb45b83b5265794aae7b4a538a14233cf91e
nicolasrls <nicolaufacul@gmail.com>
2020-02-24 16:03:24 -0300

    Atualizando o Semestre

--- Circuitos Lógicos/FPGA/meio somador 1 bit/simulation/qsim/work/meio_somador_1bit_vlg_vec_tst/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity meio_somador_1bit_vlg_vec_tst is
-end meio_somador_1bit_vlg_vec_tst;
--- Circuitos Lógicos/FPGA/meudisplay/simulation/qsim/work/meudisplay_vlg_vec_tst/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity meudisplay_vlg_vec_tst is
-end meudisplay_vlg_vec_tst;
